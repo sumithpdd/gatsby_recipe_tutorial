888aca7f39cc14cdfad6a02f7fb00ba96c29ee06
Sumith Damodaran <sumithpd@gmail.com>
2021-10-23 21:51:14 +0100

    Site medata data in config

--- SimplyRecipes/gatsby-config.js
+++ SimplyRecipes/gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Sumith's gatsby recipes site",
+    author: "@sumithpdd",
+    person: { name: "sumith", age: 42 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "sumith", age: 42 },
+      { name: "susan", age: 21 },
+    ],
+  },
   plugins: [`gatsby-plugin-styled-components`,
   `gatsby-plugin-image`,
   `gatsby-plugin-sharp`,
