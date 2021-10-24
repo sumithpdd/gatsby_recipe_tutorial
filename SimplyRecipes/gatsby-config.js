4f5aeef236ddcb188c245c3ce576c739588021a5
Sumith Damodaran <sumithpd@gmail.com>
2021-10-24 12:20:07 +0100

    testing images in a folder for Gallery

--- SimplyRecipes/gatsby-config.js
+++ SimplyRecipes/gatsby-config.js
@@ -20,5 +20,12 @@ module.exports = {
   plugins: [`gatsby-plugin-styled-components`,
   `gatsby-plugin-image`,
   `gatsby-plugin-sharp`,
-  `gatsby-transformer-sharp`],
+  `gatsby-transformer-sharp`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/assets/images`,
+    },
+  }]
 }
--- SimplyRecipes/src/pages/index.js
+++ SimplyRecipes/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react" 
 import Layout from '../components/Layout'
 import { StaticImage } from "gatsby-plugin-image"
+import Gallery from "../examples/Gallery"
 
 export default function Home() {
   return <Layout>
@@ -21,6 +22,7 @@ export default function Home() {
           </div>
         </header> 
         </main>
+        <Gallery></Gallery>
     </Layout>
     
 }
